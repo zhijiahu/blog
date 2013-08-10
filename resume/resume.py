6dff52000640eeac9f62006034a7d95c0d8427e5
Lee Ming Lim <z.jia.hu@gmail.com>
2013-08-10 19:14:27 +0800

    initial use of datastore

--- resume/resume.py
+++ resume/resume.py
@@ -1,11 +1,24 @@
 from base import BaseHandler
 from resumeData import *
+import datetime
+import logging
 
 class ResumeHandler(BaseHandler):
 	def get(self):
 		self.render("resume.html", 
 			objective = ResumeManager().getObjective(),
 			experienceList = ResumeManager().getExperienceList())
+
+	def post(self):
+		event = ResumeEvent(
+			eventType = "Experience",
+			company = "-",
+			project = "-",
+			startDate = datetime.date.today(),
+			summary = "-")
+
+		event.put()
+
 
 class ResumeManager():
 	def getObjective(self):
@@ -14,14 +27,6 @@ class ResumeManager():
 	def getExperienceList(self):
 		experienceList = []
 
-		ubisoftExperience = ResumeEvent(
-			"Ubisoft Singapore",
-			"Singapore", 
-			"Programmer",
-			datetime.date(2011,5,10), 
-			None,
-			['What did i do?', 'Something...'])
-
-		experienceList.append(ubisoftExperience)
+		experienceList = ResumeEvent.all().filter("eventType = ", "Experience")
 
 		return experienceList
--- resume/resumeData.py
+++ resume/resumeData.py
@@ -1,25 +1,12 @@
 import datetime
+from google.appengine.ext import db
 
-class Resume():
-	def __init__(self, objective, experienceList, skillsList, projectList, educationList, interests):
-		self.objective = objective
-		self.experienceList = experienceList
-		self.skillsList = skillsList
-		self.projectList = projectList
-		self.educationList = educationList
-		self.interests = interests
-
-class ResumeEvent():
-	def __init__(self, company, location, jobTitle, startDate, endDate, summaryList):
-		self.company = company
-		self.location = location
-		self.jobTitle = jobTitle
-		self.startDate = startDate
-		self.startDateText = startDate.strftime("%B %Y")
-		if not endDate:
-			self.endDate = datetime.date.today()
-			self.endDateText = "Present"
-		else:
-			self.endDate = endDate
-			self.endDateText = endDate.strftime("%B %Y")
-		self.summaryList = summaryList
+class ResumeEvent(db.Model):
+	eventType = db.StringProperty(required = True)
+	company = db.StringProperty(required = True)
+	location = db.StringProperty(default = "Singapore")
+	project = db.StringProperty(required = True)
+	role = db.StringProperty(default = "Programmer")
+	startDate = db.DateProperty(required = True)
+	endDate = db.DateProperty()
+	summary = db.TextProperty(required = True)
